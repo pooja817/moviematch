ad943485e7d8fe0f047c3fcfd2564ec675515b24
Nallabothula Pooja <97442822+pooja817@users.noreply.github.com>
2024-02-12 18:56:14 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { getTrendingMovies } from '@/utils/requests'
+import Card from './components/Card';
+
+export default async function HomePage() {
+  const movies = await getTrendingMovies();
+
+  return (
+    <div className='container my-3'>
+      <h1>Top Trending Movies</h1>
+      <div className='d-flex flex-wrap gap-3'>
+        {movies.map(movie => {
+          return <Card movie={movie}></Card>
+        })}
+      </div>
+    </div>
+  )
+}
